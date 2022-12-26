23af33aa3f9917231e812622f66745db57ce79e5
Callumf222 <callumfry57@gmail.com>
2022-12-26 19:37:55 +0000

    updated background

--- lib/main.dart
+++ lib/main.dart
@@ -163,14 +163,13 @@ class MyFlutterState extends State<MyFlutterApp> with TickerProviderStateMixin {
         body: AnimatedBackground(
         behaviour: RandomParticleBehaviour(
         options: const ParticleOptions(
-        spawnMaxRadius: 50,
-        spawnMinSpeed: 10.00,
-        particleCount: 68,
-        spawnMaxSpeed: 50,
-        minOpacity: 0.3,
-        spawnOpacity: 0.4,
-        baseColor: Colors.blue,
-        image: Image(image: AssetImage('assets/Images/Flutter.png')),
+          spawnMaxRadius: 50,
+          spawnMinSpeed: 10.00,
+          particleCount: 30,
+          spawnMaxSpeed: 40,
+          minOpacity: 0.3,
+          spawnOpacity: 0.4,
+          baseColor: Colors.blue,
       ),
     ),
 
